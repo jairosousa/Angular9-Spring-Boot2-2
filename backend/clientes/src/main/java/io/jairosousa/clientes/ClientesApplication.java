c3f7e1ab42722c5c30a1f523e7f41814c311c6c4
jaironsousa1 <jaironsousa@gmail.com>
2020-10-18 16:25:22 -0300

    Criação do projeto e classe model

---
+++ backend/clientes/src/main/java/io/jairosousa/clientes/ClientesApplication.java
@@ -0,0 +1,13 @@
+package io.jairosousa.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientesApplication.class, args);
+    }
+
+}
---
+++ backend/clientes/src/main/java/io/jairosousa/clientes/model/entity/Cliente.java
@@ -0,0 +1,26 @@
+package io.jairosousa.clientes.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+}
