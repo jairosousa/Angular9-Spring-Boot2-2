c4a4bf1ef69f99148fc0f1ec40833f98e14c94c7
jaironsousa1 <jaironsousa@gmail.com>
2020-10-19 20:37:39 -0300

    Tratando erros vindos de nosssa API

--- frontend/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ frontend/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -26,7 +26,10 @@ export class ClientesFormComponent implements OnInit {
     this.clienteService.salvar(this.cliente)
       .subscribe(response => {
         this.success = true;
-      });
+      },
+        errorResponse => this.errors = errorResponse.error.errors
+
+      );
 
   }
 
