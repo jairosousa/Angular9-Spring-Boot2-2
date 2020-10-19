dbebcdd4c84b1d391d7a6a9dd96a1f13dc776c99
jaironsousa1 <jaironsousa@gmail.com>
2020-10-19 08:06:07 -0300

    Criação do Navbar

---
+++ frontend/clientes-app/src/app/template/template.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [NavbarComponent]
+})
+export class TemplateModule { }
