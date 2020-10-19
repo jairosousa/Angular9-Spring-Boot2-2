7820c74ce74b8eb285532c7e432e50a8d024e814
jaironsousa1 <jaironsousa@gmail.com>
2020-10-19 08:13:41 -0300

    Criação do Sidebar

--- frontend/clientes-app/src/app/template/template.module.ts
+++ frontend/clientes-app/src/app/template/template.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, SidebarComponent],
   imports: [
     CommonModule
   ],
-  exports: [NavbarComponent]
+  exports: [NavbarComponent, SidebarComponent]
 })
 export class TemplateModule { }
