a2a14762a1a8b85db3cdb9b7f5b3fe5a11eb9766
jaironsousa1 <jaironsousa@gmail.com>
2020-10-20 07:13:25 -0300

    Criando o componente de listagem de clientes

--- frontend/clientes-app/src/app/clientes.service.ts
+++ frontend/clientes-app/src/app/clientes.service.ts
@@ -12,16 +12,25 @@ export class ClientesService {
     private http: HttpClient
   ) { }
 
-  getCliente(): Cliente {
-    let cliente: Cliente = new Cliente();
-    cliente.id = 1;
-    cliente.dataCadastro = '19/10/2020';
-    cliente.nome = "Fulano de tal";
-    cliente.cpf = "9999999999";
-    return cliente;
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>('http://localhost:8080/api/clientes');
   }
 
   salvar(cliente: Cliente): Observable<Cliente> {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
+
+  getClientesMoc(): Cliente[] {
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = 'Fulano';
+    cliente.cpf = '12345678912';
+    cliente.dataCadastro = '20/10/2020';
+    let cliente2 = new Cliente();
+    cliente2.id = 2;
+    cliente2.nome = 'FCiclano';
+    cliente2.cpf = '98765432198';
+    cliente2.dataCadastro = '20/10/2020';
+    return [cliente, cliente2];
   }
 }
--- frontend/clientes-app/src/app/clientes/clientes.module.ts
+++ frontend/clientes-app/src/app/clientes/clientes.module.ts
@@ -2,17 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ClientesFormComponent } from './clientes-form/clientes-form.component';
+import { ClientesListaComponent } from './clientes-lista/clientes-lista.component';
 import { ClientesRoutingModule } from './clientes-routing.module';
 
 
 
 @NgModule({
-  declarations: [ClientesFormComponent],
+  declarations: [ClientesFormComponent, ClientesListaComponent],
   imports: [
     CommonModule,
     ClientesRoutingModule,
     FormsModule
   ],
-  exports: [ClientesFormComponent]
+  exports: [ClientesFormComponent, ClientesListaComponent]
 })
 export class ClientesModule { }
