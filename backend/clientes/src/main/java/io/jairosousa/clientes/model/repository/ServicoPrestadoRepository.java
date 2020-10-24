3ea2232e94c0edf67be2aa75a08abdd9ce1e555c
jaironsousa1 <jaironsousa@gmail.com>
2020-10-24 08:58:20 -0300

    Método de pesquisa de serviços prestados

--- backend/clientes/src/main/java/io/jairosousa/clientes/model/repository/ServicoPrestadoRepository.java
+++ backend/clientes/src/main/java/io/jairosousa/clientes/model/repository/ServicoPrestadoRepository.java
@@ -2,6 +2,14 @@ package io.jairosousa.clientes.model.repository;
 
 import io.jairosousa.clientes.model.entity.ServicoPrestado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer> {
+
+    @Query("SELECT s FROM ServicoPrestado s JOIN s.cliente c Where upper(c.nome) like upper(:nome) " +
+            "AND MONTH(s.data) =:mes")
+    List<ServicoPrestado> findByNomeClienteAndMes(@Param("nome") String nome, @Param("mes") Integer mes);
 }
