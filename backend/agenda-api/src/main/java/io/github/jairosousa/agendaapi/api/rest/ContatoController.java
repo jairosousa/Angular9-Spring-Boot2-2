a86e7beb1fd3694c312a46c243e9e277be3fe193
jaironsousa1 <jaironsousa@gmail.com>
2020-11-14 11:38:08 -0300

    Desenvolvendo api de contatos

---
+++ backend/agenda-api/src/main/java/io/github/jairosousa/agendaapi/api/rest/ContatoController.java
@@ -0,0 +1,45 @@
+package io.github.jairosousa.agendaapi.api.rest;
+
+import io.github.jairosousa.agendaapi.model.entity.Contato;
+import io.github.jairosousa.agendaapi.repository.ContatoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/contatos")
+public class ContatoController {
+
+    private final ContatoRepository repository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Contato save(@RequestBody Contato contato) {
+        return repository.save(contato);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+    @GetMapping
+    public List<Contato> list() {
+        return repository.findAll();
+    }
+
+    @PatchMapping("{id}/favorito")
+    public void favorite(@PathVariable Long id, @RequestBody Boolean favorito) {
+        Optional<Contato> contato = repository.findById(id);
+
+        contato.ifPresent(c -> {
+            c.setFavorito(favorito);
+            repository.save(c);
+        });
+    }
+}
